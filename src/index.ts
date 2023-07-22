df036ca799cb9f6d7ffd6212fe0d08680e8ca63d
Jorge Castro <david92_jc@yahoo.es>
2023-07-22 17:01:31 -0500

    correcciones menores

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,9 @@ const app = express()
 const PORT = 5000
 
 app.get("/", (req, res) => {
-    console.log(`Servidor escuchando en el puerto ${PORT}`);
     res.send("Hello World!")
 })
 
-app.listen(PORT)
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`)
+})
